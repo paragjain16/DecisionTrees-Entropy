b901b1b70841d7a532be385ce30968ceeb639839
paragjain16 <paragjain16@gmail.com>
2014-10-13 22:18:50 -0500

    some changes

--- src/EntropyCalc_pjain11.java
+++ src/EntropyCalc_pjain11.java
@@ -31,6 +31,7 @@ public class EntropyCalc_pjain11 {
                     initial = false;
                     for(String value: values){
                         ArrayList<String> column = new ArrayList<String>();
+                        column.add(value);
                         columns.add(column);
                     }
                 }else{
@@ -61,9 +62,11 @@ public class EntropyCalc_pjain11 {
         Iterator<Map.Entry<String, Integer>> it = distinctEntries.entrySet().iterator();
         while(it.hasNext()){
             Map.Entry<String, Integer> entry = it.next();
+            System.out.println(entry.getKey() + " "+ entry.getValue());
             double probability = (double)entry.getValue()/totalEntries;
-            entropy += probability*(Math.log(probability)/Math.log(2))*-1.0;
+            entropy += (probability*(Math.log(probability)/Math.log(2))*(-1.0));
         }
+        System.out.println(entropy);
         return entropy;
     }
 
